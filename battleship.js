9ba73e066d55f9867d10dd9a189449f8b44d1534
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2020-05-23 10:42:40 +0000

    Create start game overlay

--- battleship.js
+++ battleship.js
@@ -191,3 +191,21 @@ function init() {
 	
 	model.generateShipLocations();
 }
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+        });
+    });
+
+}
